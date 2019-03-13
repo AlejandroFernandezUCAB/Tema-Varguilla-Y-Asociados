da20578e873fb96c546e22a1bb965608c55602cc
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2019-03-13 16:20:33 -0400

    Agregando imageenes y fotos para articulos

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,4 +1,8 @@
-/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
-$('.carousel').carousel({
-    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
-});
+(function($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+    
+})(jQuery);
