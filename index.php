51d4aa55ee628dff5b71cf822a04c3b50ffbcb9a
Pedro Fernandez <pedropaff7@gmail.com>
2019-03-16 13:16:56 -0400

    Arreglando el codigo

--- index.php
+++ index.php
@@ -1,126 +1,121 @@
 <?php get_header();?>
 
 <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-						<ol class="carousel-indicators">
-							<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-							<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-							<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-						</ol>
-						<div class="carousel-inner" role="listbox">
-							<div class="carousel-item active" style="background-image: url('<?php bloginfo( 'template_url')?>/images/slider1.jpg')">
-								<div class="carousel-caption d-none d-md-block">
-									<h5>Asesoría legal</h5>
-									<p>Lorem ipsum dolor sit amet</p>
-								</div>
-							</div>
-							<div class="carousel-item" style="background-image: url('<?php bloginfo( 'template_url')?>/images/slider2.png')">
-								<div class="carousel-caption d-none d-md-block">
-									<h5>Especializaciones</h5>
-									<p>Lorem ipsum dolor sit amet</p>
-								</div>
-							</div>
-							<div class="carousel-item" style="background-image: url('<?php bloginfo( 'template_url')?>/images/slider3.png')">
-								<div class="carousel-caption d-none d-md-block">
-									<h5>Atención rápida y clara</h5>
-									<p>Lorem ipsum dolor sit amet</p>
-								</div>
-							</div>
-						</div>
-						<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-							<span class="sr-only">Anterior</span>
-						</a>
-						<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-							<span class="carousel-control-next-icon" aria-hidden="true"></span>
-							<span class="sr-only">Siguiente</span>
-						</a>
-					</div>			
+	<ol class="carousel-indicators">
+		<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+		<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+		<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner" role="listbox">
+		<div class="carousel-item active" style="background-image: url('<?php bloginfo( 'template_url')?>/images/slider1.jpg')">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Asesoría legal</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+			</div>
+		</div>
+		<div class="carousel-item" style="background-image: url('<?php bloginfo( 'template_url')?>/images/slider2.png')">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Especializaciones</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+			</div>
+		</div>
+		<div class="carousel-item" style="background-image: url('<?php bloginfo( 'template_url')?>/images/slider3.png')">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Atención rápida y clara</h5>
+					<p>Lorem ipsum dolor sit amet</p>
+			</div>
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Anterior</span>
+	</a>
+	<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">Siguiente</span>
+	</a>
+</div>			
 			
-			<!-- Page Content -->
-			<section class="py-5">
-				
-				<div class="container">
-
-						<h3 class="text-center">Noticias relevantes</h3>
-						
-						<div class="card-group text-center py-4">
-								<div class="card">
-										<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-										<a href="#" class="btn btn-primary mx-auto">Ver más</a>
-									</div>
-									<div class="card-footer">
-										<small class="text-muted">Last updated 3 mins ago</small>
-									</div>
-								</div>
-								<div class="card">
-										<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-										<a href="#" class="btn btn-primary">Ver más</a>
-									</div>
-									<div class="card-footer">
-										<small class="text-muted">Last updated 3 mins ago</small>
-									</div>
-								</div>
-								<div class="card">
-										<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-										<a href="#" class="btn btn-primary">Ver más</a>
-									</div>
-									<div class="card-footer">
-										<small class="text-muted">Last updated 3 mins ago</small>
-									</div>
-								</div>
-								
-							</div>
+<!-- Page Content -->
+<section class="py-5">
+	<div class="container">
+		<h3 class="text-center">Noticias relevantes</h3>		
+		
+		<div class="card-group text-center py-4">
+			<div class="card">
+				<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<a href="#" class="btn btn-primary mx-auto">Ver más</a>
+					</div>
+				<div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
 				</div>
-
-				<div class="container">
-
-						<h3 class="text-center">Últimas Noticias</h3>
-						
-						<div class="card-group text-center py-4">
-								<div class="card">
-										<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-										<a href="#" class="btn btn-primary mx-auto">Ver más</a>
-									</div>
-									<div class="card-footer">
-										<small class="text-muted">Last updated 3 mins ago</small>
-									</div>
-								</div>
-								<div class="card">
-										<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-										<a href="#" class="btn btn-primary">Ver más</a>
-									</div>
-									<div class="card-footer">
-										<small class="text-muted">Last updated 3 mins ago</small>
-									</div>
-								</div>
-								<div class="card">
-										<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-										<a href="#" class="btn btn-primary">Ver más</a>
-									</div>
-									<div class="card-footer">
-										<small class="text-muted">Last updated 3 mins ago</small>
-									</div>
-								</div>
-								
-							</div>
+			</div>
+			<div class="card">
+				<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+						<a href="#" class="btn btn-primary">Ver más</a>
+					</div>
+					<div class="card-footer">
+						<small class="text-muted">Last updated 3 mins ago</small>
+					</div>
+			</div>
+			<div class="card">
+				<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>	
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					<a href="#" class="btn btn-primary">Ver más</a>
 				</div>
-			</section>
+				<div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div>
+			</div>
+		</div>
+</div>
+
+	<div class="container">
+		<h3 class="text-center">Últimas Noticias</h3>				
+		<div class="card-group text-center py-4">
+			<div class="card">	
+				<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<a href="#" class="btn btn-primary mx-auto">Ver más</a>
+				</div>
+				<div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div>
+			</div>
+			<div class="card">
+				<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<a href="#" class="btn btn-primary">Ver más</a>
+				</div>
+				<div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div>
+			</div>
+			<div class="card">
+				<svg class="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em"></text></svg>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					<a href="#" class="btn btn-primary">Ver más</a>
+				</div>
+				<div class="card-footer">
+					<small class="text-muted">Last updated 3 mins ago</small>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php get_footer();?>
